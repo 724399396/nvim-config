5de0bc987324dd62435654424bfa334650c73646
Wei Li <liwei72439936@gmail.com>
2023-05-16 14:32:27 +0800

    remove backseat plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -545,14 +545,4 @@ return {
       })
     end,
   },
-
-  {
-    "james1236/backseat.nvim",
-    config = function()
-      require("backseat").setup({
-        openai_model_id = "gpt-3.5-turbo", --gpt-4 (If you do not have access to a model, it says "The model does not exist")
-        -- additional_instruction = "Respond snarkily", -- (GPT-3 will probably deny this request, but GPT-4 complies)
-      })
-    end,
-  },
 }
