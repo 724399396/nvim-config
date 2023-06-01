3b9735b9565350056017012d8e221ffc8d492f98
Wei Li <liwei72439936@gmail.com>
2023-06-01 19:19:04 +0800

    add csharp config

---
+++ lua/plugins/extras/lang/csharp.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "c_sharp" })
+      end
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        omnisharp_mono = {},
+      },
+    },
+  },
+
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "coreclr" })
+    end,
+  },
+}
