18ed7dbb954eada1ccfd66ce86c34facffc63213
Wei Li <liwei72439936@gmail.com>
2023-06-30 14:07:26 +0800

    neo tree add yank name short cut

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,4 +6,19 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
+  opt = {
+    filesystem = {
+      commands = {
+        copy_file_name = function(state)
+          local node = state.tree:get_node()
+          vim.fn.setreg("*", node.name, "c")
+        end,
+      },
+      window = {
+        mappings = {
+          ["Y"] = "copy_file_name",
+        },
+      },
+    },
+  },
 }
