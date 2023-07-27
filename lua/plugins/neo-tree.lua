005cac15bc1711f8195406309550821906d17247
Wei Li <liwei72439936@gmail.com>
2023-07-27 21:28:32 +0800

    fix neo-tree setup key

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,19 +6,21 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  opt = {
-    filesystem = {
-      commands = {
-        copy_file_name = function(state)
-          local node = state.tree:get_node()
-          vim.fn.setreg("*", node.name, "c")
-        end,
-      },
-      window = {
-        mappings = {
-          ["Y"] = "copy_file_name",
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        commands = {
+          copy_file_name = function(state)
+            local node = state.tree:get_node()
+            vim.fn.setreg("*", node.name, "c")
+          end,
+        },
+        window = {
+          mappings = {
+            ["Y"] = "copy_file_name",
+          },
         },
       },
-    },
-  },
+    })
+  end,
 }
