9ae16f563042f3aa1714944149f1361384d266a6
Wei Li <liwei72439936@gmail.com>
2023-07-29 10:14:35 +0800

    update neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
+  branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -12,6 +12,15 @@ return {
         follow_current_file = {
           enabled = true,
           leave_dirs_open = false,
+        },
+        filtered_items = {
+          always_show = {
+            ".gitignore",
+            ".gitlab-ci.yml",
+            ".env",
+            ".env.development",
+            ".env.production",
+          },
         },
         commands = {
           copy_file_name = function(state)
