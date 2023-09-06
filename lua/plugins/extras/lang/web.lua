e87682c29d51031c5b5edba696bce4a63c53e795
Wei Li <liwei72439936@gmail.com>
2023-09-06 18:55:35 +0800

    去除prettierd, 使用lsp的format

--- lua/plugins/extras/lang/web.lua
+++ lua/plugins/extras/lang/web.lua
@@ -9,24 +9,6 @@ return {
   },
 
   {
-    "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "prettierd" })
-      end
-    end,
-  },
-
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "prettierd",
-      },
-    },
-  },
-
-  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
