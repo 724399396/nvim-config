3a21e8222399d09c674c2c946478f24a14c41780
Wei Li <liwei72439936@gmail.com>
2023-09-13 10:01:46 +0800

    add watchman fix

--- init.lua
+++ init.lua
@@ -1,46 +1,47 @@
 require("config.options")
 require("config.keymaps")
 require("config.autocommands")
+require("config.fix")
 
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-		{ import = "plugins.lsp" },
-		{ import = "plugins.dap" },
-		{ import = "plugins.extras.lang" },
-	},
+  spec = {
+    { import = "plugins" },
+    { import = "plugins.lsp" },
+    { import = "plugins.dap" },
+    { import = "plugins.extras.lang" },
+  },
 }, {
-	checker = { enabled = true }, -- automatically check for plugin updates
-	install = { colorscheme = { "tokyonight" } },
-	performance = {
-		rtp = {
-			-- disable some rtp plugins
-			disabled_plugins = {
-				"gzip",
-				-- "matchit",
-				-- "matchparen",
-				-- "netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
+  checker = { enabled = true }, -- automatically check for plugin updates
+  install = { colorscheme = { "tokyonight" } },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
 })
