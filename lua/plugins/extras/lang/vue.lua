c3d600fdcf86b313a0b01b5f221f751f39779277
Wei Li <liwei72439936@gmail.com>
2023-11-09 11:57:30 +0800

    fix json format

--- lua/plugins/extras/lang/vue.lua
+++ lua/plugins/extras/lang/vue.lua
@@ -23,7 +23,7 @@ return {
     opts = {
       servers = {
         volar = {
-          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
           on_new_config = function(new_config, new_root_dir)
             new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
           end,
