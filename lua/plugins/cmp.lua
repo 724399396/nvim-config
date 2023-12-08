d553b80afaafbc617dbfc2c4ae35795296b52779
Wei Li <liwei72439936@gmail.com>
2023-12-08 17:57:56 +0800

    update luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,6 +14,8 @@ return {
       "saadparwaiz1/cmp_luasnip",
       {
         "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
         dependencies = { "rafamadriz/friendly-snippets" },
       },
       {
